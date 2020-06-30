f257b59253dcfb074239dbe19a0f76ddf20ac219
Rojalin Mahalik <rojalin.mahalik@mountblue.tech>
2020-06-30 14:17:53 +0530

    Modify app.js

--- task-3/src/App.js
+++ task-3/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
 import TableContainer from './components/tableContainer';
 import TableContextProvider from './contexts/TableContext';
 
 function App() {
   return (
-    <Provider store={store}>
-      <TableContextProvider>
-        {' '}
-        <TableContainer />
-      </TableContextProvider>
-    </Provider>
+    <TableContextProvider>
+      <TableContainer />
+    </TableContextProvider>
   );
 }
 
